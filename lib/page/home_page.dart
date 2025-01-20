b7ba4b741d7fe386d16663010ac6e360181e9a0b
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-01-20 14:54:15 +0500

    Minor Improvement

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:currency_convertor/models/currency_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:live_currency_rate/live_currency_rate.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -35,10 +35,57 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    LiveCurrencyRate.convertCurrency("USD", "VND", 1).then((res) {
-      print(res.result);
-      print(res.message);
-    });
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xffecf2f8),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                SizedBox(height: 20),
+                Text(
+                  "Currency Converter",
+                  style: GoogleFonts.roboto(
+                    color: Color(0xff1F2261),
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  "Check live rates, set rate alerts, receive notifications and more.",
+                  style: GoogleFonts.roboto(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xff808080),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xffFFFFFF),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Amount",
+                          style: GoogleFonts.roboto(
+                            fontSize: 15,
+                            color: Color(0xff989898),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
